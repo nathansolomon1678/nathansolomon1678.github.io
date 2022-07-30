772de6579cb39a516caa34886ec22f8fd68bb6c6
Nathan Solomon <nathansolomon1678@gmail.com>
2022-07-30 04:20:43 -0700

    Reorganize a bit

--- fractal_renderer_cpp/glfw_ex2.c
+++ fractal_renderer_cpp/glfw_ex2.c
@@ -7,51 +7,43 @@
 #include <stdio.h>
 #include <GLES2/gl2.h>
 #include <EGL/egl.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
  
-static const struct
-{
+static const struct {
     float x, y;
     float r, g, b;
-} vertices[3] =
-{
+} vertices[4] = {
     { -0.6f, -0.4f, 1.f, 0.f, 0.f },
     {  0.6f, -0.4f, 0.f, 1.f, 0.f },
+    {  0.6f,  0.4f, .5f, 0.f, 0.f },
     {   0.f,  0.6f, 0.f, 0.f, 1.f }
 };
+
+std::string file_contents(std::string filename) {
+    std::stringstream sstream;
+    sstream << std::ifstream(filename).rdbuf();
+    return sstream.str();
+}
+
+static std::string vertex_shader_str     = file_contents("vertex.c");
+static const char* vertex_shader_c_str   = vertex_shader_str.c_str();
+
+static std::string fragment_shader_str   = file_contents("fragment.c");
+static const char* fragment_shader_c_str = fragment_shader_str.c_str();
  
-static const char* vertex_shader_text =
-"#version 110\n"
-"uniform mat4 MVP;\n"
-"attribute vec3 vCol;\n"
-"attribute vec2 vPos;\n"
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
-"    color = vCol;\n"
-"}\n";
- 
-static const char* fragment_shader_text =
-"#version 110\n"
-"varying vec3 color;\n"
-"void main()\n"
-"{\n"
-"    gl_FragColor = vec4(color, 1.0);\n"
-"}\n";
- 
-static void error_callback(int error, const char* description)
-{
+static void error_callback(int error, const char* description) {
     fprintf(stderr, "Error: %s\n", description);
 }
  
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
  
-int main(void)
-{
+int main(void) {
     GLFWwindow* window;
     GLuint vertex_buffer, vertex_shader, fragment_shader, program;
     GLint mvp_location, vpos_location, vcol_location;
@@ -64,9 +56,8 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
  
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
-    if (!window)
-    {
+    window = glfwCreateWindow(1920, 1080, "Escape-time fractal generator", NULL, NULL);
+    if (!window) {
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
@@ -76,18 +67,16 @@ int main(void)
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
  
-    // NOTE: OpenGL error checks have been omitted for brevity
- 
     glGenBuffers(1, &vertex_buffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
  
     vertex_shader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+    glShaderSource(vertex_shader, 1, &vertex_shader_c_str, NULL);
     glCompileShader(vertex_shader);
  
     fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+    glShaderSource(fragment_shader, 1, &fragment_shader_c_str, NULL);
     glCompileShader(fragment_shader);
  
     program = glCreateProgram();
@@ -105,9 +94,8 @@ int main(void)
     glEnableVertexAttribArray(vcol_location);
     glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
                           sizeof(vertices[0]), (void*) (sizeof(float) * 2));
- 
-    while (!glfwWindowShouldClose(window))
-    {
+
+    while (!glfwWindowShouldClose(window)) {
         float ratio;
         int width, height;
         mat4x4 m, p, mvp;
@@ -125,7 +113,7 @@ int main(void)
  
         glUseProgram(program);
         glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat*) mvp);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
  
         glfwSwapBuffers(window);
         glfwPollEvents();
---
+++ fractal_renderer_cpp/vertex.c
@@ -0,0 +1,11 @@
+#version 110
+
+uniform mat4 MVP;
+attribute vec3 vCol;
+attribute vec2 vPos;
+varying vec3 color;
+
+void main() {
+    gl_Position = MVP * vec4(vPos, 0., 1.);
+    color = vCol;
+}
